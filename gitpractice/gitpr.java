7d3d99b0b0a13629be3989b1adab497e5fc3c6c1
vyshnavi <tallamvyshnavigangothri.com>
2025-12-02 23:25:10 +0530

    Updated java file

--- gitpractice/gitpr.java
+++ gitpractice/gitpr.java
@@ -1,5 +1,6 @@
 public class gitpr {
     public static void main(String[] args) {
         System.out.println("Hello Git and GitHub!");
+        System.out.println("This is a practice for pull requests.");
     }
 }
